c3a6bdfc374bda2166839920a987830a4558f049
Dovile Tumaite <dovile.tumaite@yahoo.it>
2021-12-28 16:42:18 +0100

    up

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,79 @@
 import MineNavbar from "./MineNavbar"
 import MyFooter from "./MyFooter"
 import MyCard from "./MyCard"
-import { Container, Row, Col } from "react-bootstrap"
-import MyJumbotron from "./MyJumbotron"
-import scifiBooks from"../json/scifi.json"
+import { Container, Row, Col, DropdownButton,Dropdown, InputGroup, Jumbotron, FormControl} from "react-bootstrap"
+import { useEffect } from "react"
+import { useState } from "react"
+import scifi from "../json/scifi.json"
+import fantasy from "../json/fantasy.json"
+import history from "../json/history.json"
+import horror from "../json/horror.json"
+import romance from "../json/romance.json"
 
 const Home = () => {
-    const scifiBooksArray = scifiBooks
+    const [bookArray, setBookArray] = useState([])
+    useEffect(() => {
+    setBookArray(scifi)
+    }, [])
+    
+    const [scifiArray, setScifiArray] = useState([])
+    const [fantasyArray, setFantasyArray] = useState([])
+    const [historyArray, setHistoryArray] = useState([])
+    const [horrorArray, setHorrorArray] = useState([])
+    const [romanceArray, setRomanceArray] = useState([])
+    const [searchResult, setSearchResult] = useState([])
+    useEffect(() => {
+    setScifiArray(scifi)
+    setFantasyArray(fantasy)
+    setHistoryArray(history)
+    setHorrorArray(horror)
+    setRomanceArray(romance)
+    console.log("array",scifiArray)
+    }, [])
    
+    const [query, setQuery] = useState("")
+    
+    const handleQuery = (e) => {
+        setQuery(e.target.value)
+        console.log("query", query)
+        const result = scifiArray.filter(b=>b.toLowerCase().includes(query.toLowerCase()) )
+        console.log("result", searchResult)
+          setSearchResult(result)
+    }
     return (
         <div>
             <MineNavbar/>
             <Container>
-                <MyJumbotron/>
+            <Jumbotron fluid className="my-5">
+  <Container>
+    <h1>Strive Books</h1>
+    <p>
+    Find out your next book
+    </p>
+    <InputGroup className="mb-3">
+    <DropdownButton
+      as={InputGroup.Prepend}
+      variant="outline-secondary"
+      title="choose a catagory"
+      id="input-group-dropdown-1"
+    >
+      <Dropdown.Item href="#">Scifi</Dropdown.Item>
+      <Dropdown.Item href="#">Fantasy</Dropdown.Item>
+      <Dropdown.Item href="#">History</Dropdown.Item>
+      <Dropdown.Item href="#">Horror</Dropdown.Item>
+      <Dropdown.Item href="#">Romance</Dropdown.Item>
+    </DropdownButton>
+    <FormControl aria-describedby="basic-addon1" value={query} onChange={(e)=>handleQuery(e)}/>
+  </InputGroup>
+  </Container>
+</Jumbotron>
                 <Row className="my-5">
-                   
-            {scifiBooksArray.map(b=>
+                {searchResult ? searchResult.map(b=>
+             <Col xs={6} md={4} className="my-3">
+                 <MyCard book={b}/>
+                  </Col>
+                 ) : (<></>)}      
+            {bookArray.map(b=>
              <Col xs={6} md={4} className="my-3">
                  <MyCard book={b}/>
                   </Col>
